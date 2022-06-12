915fd65b952ba26ca97820ec3f097fa9b36e3652
MaraOD555 <ruffbc@hotmail.com>
2022-06-12 21:25:51 +0300

    Задача 2, предложить пользователю нужную версию приложения в зависимости от года выпуска девайса и ОС.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         exercise1(2022);
+        exercise2(0, 2020);
 
     }
         //Задание 1 Реализуйте метод, который
@@ -12,6 +13,24 @@ public class Main {
             System.out.println(year + " - високосный год");
         }  else {
             System.out.println(year + " - не високосный год");
+        }
+    }
+
+        //Задание 2 Напишите метод, куда подаются два параметра: тип операционной системы (ОС)
+        // ( 0 — iOS или 1 — Android ) и год выпуска устройства.
+        //Если устройство старше текущего года, предложите ему установить lite-версию (облегченную версию).
+
+    public static void exercise2 (int os, int yearOfRelease) {
+        int currentYear = 2022;
+
+        if (os == 0 && yearOfRelease < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (os == 1 && yearOfRelease < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (os == 0 && yearOfRelease >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (os == 1 && yearOfRelease >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
 
