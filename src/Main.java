def1a799439ed12d4042cbaf1c8db644c3793966
MaraOD555 <ruffbc@hotmail.com>
2022-06-12 21:00:59 +0300

    Задача 1, метод получающий в виде параметра год ( проверка високосный или нет)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        exercise1(2022);
+
     }
+        //Задание 1 Реализуйте метод, который
+        // получает в качестве параметра год, а затем проверяет,
+        // является ли он високосным, и выводит результат в консоль.
+
+    public static void exercise1 (int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " - високосный год");
+        }  else {
+            System.out.println(year + " - не високосный год");
+        }
+    }
+
 }
